3ad236ebb777c6f71c661d4f6b35710c3625a327
Margo <m.yunanova@gmail.com>
2023-06-07 22:56:44 +0300

    refactoring

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
 const page = document.querySelector(".page");
-const cards = page.querySelectorAll(".app-description");
-const cardsPromo = page.querySelectorAll(".app-promo__images");
+const cards = page.querySelectorAll(".app-description, .app-promo__images");
 
 function scrollHandler(event) {
   const numberCards = event.target.children.length;
-  const scrollWidth = event.target.scrollWidth;
-  const scrollLeft = event.target.scrollLeft;
+  const { scrollWidth, scrollLeft } = event.target;
   const activeCard = Math.round(scrollLeft / (scrollWidth / numberCards));
-  const dots =
-    event.target.nextElementSibling.querySelectorAll(".description__dot");
+  const dots = event.target.nextElementSibling.querySelectorAll(".scroll_dot");
 
   dots.forEach((dot, index) => {
-    if (index === activeCard) {
-      dot.style.backgroundColor = "#212121";
-    } else {
-      dot.style.backgroundColor = "#D9D9D9";
-    }
+    dot.style.backgroundColor = index === activeCard ? "#212121" : "#D9D9D9";
   });
 }
 
-cards.forEach((block) => {
-  block.addEventListener("scroll", scrollHandler);
-});
-
-cardsPromo.forEach((block) => {
-  block.addEventListener("scroll", scrollHandler);
+cards.forEach((card) => {
+  card.addEventListener("scroll", scrollHandler);
 });
